cf4be55b58df63bc6d51e4cd9ebf1f5fcd0c9c77
r-bierman <rbierman@stanford.edu>
2016-09-01 21:24:17 -0700

    deleted large spork glm files, fixed wrapper

--- SPACHETE_glm_wrapper.sh
+++ SPACHETE_glm_wrapper.sh
@@ -16,6 +16,65 @@
 KNIFE_DIR="/scratch/PI/horence/gillian/Ewing/circpipe"
 OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/ewing_9_1"
 
+#############################
+#    CML UConn samples      #
+#############################
+#KNIFE_DIR="/scratch/PI/horence/gillian/CML_UConn/circpipe_K562"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/CML_uconn_8_31/"
+#STEM_INCLUDE_ONLY_LIST=("SRR3192410")
+
+#############################
+#     CML test samples      #
+#############################
+#KNIFE_DIR="/scratch/PI/horence/gillian/CML_test/aligned/CML"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/cml_test_8_25"
+
+#################################
+#    Normal breast samples      #
+#################################
+#KNIFE_DIR="/scratch/PI/horence/gillian/normal_breast/circpipe"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/normal_breast_9_1_36-cutoff-glm"
+#STEM_INCLUDE_ONLY_LIST=("SRR1027188")
+
+######################################
+#    Simulated engstrom samples      #
+###############################3######
+#KNIFE_DIR="/scratch/PI/horence/gillian/Engstrom/circpipe_engstrom"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/gillian/engstrom"
+
+################################
+#    Normal fetal samples      #
+################################
+#KNIFE_DIR="/scratch/PI/horence/gillian/normal_fetal/circpipe_fetal"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/normal_fetal_8_31"
+#STEM_INCLUDE_ONLY_LIST=("Fetal_Heart_397_CAGATC_L004")
+
+################################
+#        Bladder samples       #
+################################
+#KNIFE_DIR="/scratch/PI/horence/gillian/bladder/circpipe"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/bladder_8_27"
+
+#############################
+#      RNaseR samples       #
+#############################
+#KNIFE_DIR="/scratch/PI/horence/gillian/ov_RNaseR_Qatar/ovCircPipe"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/gillian/RNaseR"
+
+#############################
+#    Prostate samples       #
+#############################
+#KNIFE_DIR="/scratch/PI/horence/gillian/prostate/aligned/circpipe"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/gillian/prostate"
+
+#############################
+#        SEQC study         #
+#############################
+#KNIFE_DIR="/scratch/PI/horence/gillian/SEQC_study_set/circpipe_SEQC"
+#OUT_DIR="/scratch/PI/horence/rob/spachete_outputs/gillian/seqc"
+
+
+
 
 
 ######################################################################################
